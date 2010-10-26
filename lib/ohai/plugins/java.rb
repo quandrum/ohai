a08c716aba463d0bdda66b79ffe5fd9e84c8cb0c
Seth Chisamore <schisamo@opscode.com>
2010-10-26 00:54:53 -0400

    added extra match for openjdk runtime strings

--- lib/ohai/plugins/java.rb
+++ lib/ohai/plugins/java.rb
@@ -30,6 +30,8 @@ if status == 0
       java[:version] = $1
     when /^(.+Runtime Environment.*) \(build (.+)\)$/
       java[:runtime] = { "name" => $1, "build" => $2 }
+    when /^(.+Runtime Environment.*) \((.+)\)$/
+      java[:runtime] = { "name" => $1, "build" => $2 }
     when /^(.+ Client VM) \(build (.+)\)$/ 
       java[:hotspot] = { "name" => $1, "build" => $2 }
     end
